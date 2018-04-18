2cc39596cbc2c56e29b705b3fd14ac9e366e6e1d
Tushar Verma <tushar.m.verma@gmail.com>
2018-04-18 09:26:55 -0500

    Main file for testing

--- radixSortHW/main.cpp
+++ radixSortHW/main.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include "radixSort.h"
+#include <chrono>
+using namespace std;
 
+
+int main()
+{
+    radixSort s;
+    int base;
+    cout <<"Enter the Base: ";
+    cin>> base;
+    s.getdata();
+    s.radixsort(base);
+    s.showdata();
+    return 0;
+}
